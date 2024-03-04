3b7cac13de95ea00197fce09609598575872b5dc
GoodPointt <dev6012@gmail.com>
2024-03-04 15:49:10 +0200

    fix: improve search logic, to clear input when store changes

--- client/src/components/Search.tsx
+++ client/src/components/Search.tsx
@@ -1,15 +1,23 @@
 import { Input, InputGroup, InputLeftElement } from '@chakra-ui/react';
+import { useEffect, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { useDebouncedCallback } from 'use-debounce';
 
 const Search = () => {
-  const [, setSearchParams] = useSearchParams();
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const handleSearch = useDebouncedCallback((e) => {
+  const [inputValue, setInputValue] = useState('');
+
+  useEffect(() => {
+    const searchValue = searchParams.get('search') || '';
+    setInputValue(searchValue);
+  }, [searchParams]);
+
+  const handleSearch = useDebouncedCallback((value) => {
     const currentSearchParams = new URLSearchParams(location.search);
-    if (e.target.value) {
+    if (value) {
       currentSearchParams.delete('count');
-      currentSearchParams.set('search', e.target.value);
+      currentSearchParams.set('search', value);
       currentSearchParams.set('page', '1');
       setSearchParams(currentSearchParams.toString());
     } else {
@@ -18,6 +26,7 @@ const Search = () => {
       setSearchParams(currentSearchParams.toString());
     }
   }, 300);
+
   return (
     <InputGroup ml={{ base: 'none', lg: 'auto' }} w={'255px'}>
       <InputLeftElement pointerEvents="none" color="gray.300" fontSize="1.2em">
@@ -26,11 +35,14 @@ const Search = () => {
       <Input
         px={2}
         py={'5px'}
+        value={inputValue}
         variant={'unstyled'}
         placeholder="Search by medicine name..."
         type="search"
         onChange={(e) => {
-          handleSearch(e);
+          const { value } = e.target;
+          setInputValue(value);
+          handleSearch(value);
         }}
       />
     </InputGroup>
